90acae3d9f920927ddf969aa7a2fbfb5131153e5
Debarshi <debarshi121@gmail.com>
2022-08-11 17:15:42 +0530

    yt

--- app/Http/Controllers/YoutubeController.php
+++ app/Http/Controllers/YoutubeController.php
@@ -3,21 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Symfony\Component\Process\Process;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 
 class YoutubeController extends Controller
 {
     public function download(Request $request)
     {
-        $cmd = "yt-dlp --extract-audio --audio-format mp3 ".$request->videoUrl;
+        ini_set('max_execution_time', '300');
 
-        return file_get_contents($request->videoUrl);
+        $uuid = (string) Str::uuid();
+
+        $cmd = "cd storage/downloads && youtube-dl --write-info-json --write-sub --write-thumbnail -o " . $uuid . "/" . $uuid . ".%(ext)s -x --audio-format mp3 " . $request->videoUrl;
 
         try {
-            return shell_exec($cmd);
+            if (shell_exec($cmd)) {
+                Log::info($uuid);
+                $data = json_decode(file_get_contents("storage/downloads/".$uuid."/".$uuid.".info.json"), true);
+                return $data['fulltitle'];
+            }
         } catch (\Throwable $exception) {
             return $exception;
         }
-        
     }
 }
